b9fdbb98f93b5ea782cbc763e40eb4e83f8a6966
Adrians Raitums <126756100+bigboy3302@users.noreply.github.com>
2025-09-17 15:03:29 +0300

    admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ChallengeSeeder::class,
             CategorySeeder::class,
+            AdminUserSeeder::class,
         ]);
 
 
