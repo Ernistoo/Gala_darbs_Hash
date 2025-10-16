dc60fae3b9feefba638f165634e5dbd5f33efb6e
Adrians Raitums <126756100+bigboy3302@users.noreply.github.com>
2025-10-16 09:30:41 +0300

    bakc poga

--- resources/views/posts/category.blade.php
+++ resources/views/posts/category.blade.php
@@ -8,12 +8,16 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             
-            <div class="mb-4">
-                <a href="{{ route('posts.create') }}"
-                   class="inline-flex items-center justify-center w-12 h-12 bg-transparent rounded-full shadow hover:bg-purple-400 transition">
-                    <img src="{{ asset('images/add.svg') }}" alt="Create Post" class="w-6 h-6">
-                </a>
-            </div>
+        <div class="mb-4">
+    <a href="{{ route('posts.create') }}"
+       class="inline-flex items-center justify-center w-12 h-12 
+              bg-purple-500 dark:bg-transparent 
+              rounded-full shadow 
+              hover:bg-purple-400 dark:hover:bg-purple-600 
+              transition">
+        <img src="{{ asset('images/add.svg') }}" alt="Create Post" class="w-6 h-6">
+    </a>
+</div>
 
             @if($posts->count())
                 <div class="columns-2 sm:columns-3 md:columns-4 lg:columns-5 gap-4 space-y-4">
